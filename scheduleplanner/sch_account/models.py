0e8a5cc33dc153f5298c8cb232952b33528345b1
李宵逸 <13691419520@163.com>
2019-02-09 20:44:31 -0800

    .

--- scheduleplanner/sch_account/models.py
+++ scheduleplanner/sch_account/models.py
@@ -12,7 +12,22 @@ class UserAccount(models.Model):
 		return self.basic.email
 
 ClassInfo = {
-	
+	'ECS20': {'desc': 'Discrete Mathematics for Computer Science', 'preq': ['MAT21A']},
+	'ECS36A': {'desc': 'Programming and Problem Solving', 'preq': []},
+	'ECS36B': {'desc': 'Software Development and Object-Oriented Programming', 'preq': ['ECS36A']},
+	'ECS50': {'desc': 'Computer Organization and Machine-Dependent Programming', 'preq': ['ECS36B']},
+	'ECS36C': {'desc': 'Data Structures, Algorithms, and Programming', 'preq': ['ECS36B', 'ECS20']},
+	'MAT21A': {'desc': 'Caculus', 'preq': []},
+	'MAT21B': {'desc': 'Caculus', 'preq': ['MAT21A']},
+	'MAT21C': {'desc': 'Caculus', 'preq': ['MAT21B']},
+	'MAT22A': {'desc': 'Linear Algebra', 'preq': ['MAT21C']},
+	'ECS132': {'desc': 'Probability and Statistical Modeling for Computer Science', 'preq': ['ECS36B', 'ECS50', 'MAT21C', 'MAT22A']},
+	'ECS120': {'desc': 'Theory of Computation', 'preq': ['ECS20']},
+	'ECS122A': {'desc': 'Algorithm Design and Analysis', 'preq': ['ECS20', 'ECS36C']},
+	'ECS122B': {'desc': 'Algorithm Design and Analysis', 'preq': ['ECS122A', 'ECS36C']},
+	'ECS140A': {'desc': 'Programming Languages', 'preq': ['ECS50', 'ECS36C', 'ECS20']},
+	'ECS150': {'desc': 'Operating Systems and System Programming', 'preq': ['ECS36C', 'ECS154A']},
+	'ECS154A': {'desc': 'Computer Architecture', 'preq': ['ECS50']},
 }
 
 class UserClass(models.Model) :
